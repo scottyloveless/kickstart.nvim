9627b7519adb903d1be8f00727e8817912aaa03e
Scotty Loveless <8176125+scottyloveless@users.noreply.github.com>
2025-02-19 14:51:50 -0600

    added transparency to tokyonight-section.lua

--- tokyonight-section.lua
+++ tokyonight-section.lua
@@ -3,8 +3,11 @@
     config = function()
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
+        transparent = true,
         styles = {
           comments = { italic = false }, -- Disable italics in comments
+          sidebars = 'transparent',
+          floats = 'transparent',
         },
         on_colors = function(colors)
           colors.bg = '#011628'
